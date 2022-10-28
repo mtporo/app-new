34b24fc5d8a5082ad809c8746422707628acbe79
mtporo <94977178+mtporo@users.noreply.github.com>
2022-10-28 16:03:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>AHMED ZAIDI</title>
+</head>
+<body>
+
+    <header>
+        <a href="#" class="logo">أحمد الزيدي</a>
+        <nav class="navigation">
+            <!-- <a href="#services">خدمات</a> -->
+            <a href="https://wa.me/212671272638">إستشر الان !</a>
+            <a href="#contact">تواصل معنا</a>
+        </nav>
+    </header>
+
+
+    <section class="main">
+        <div>
+           <!-- <h2>Hello, I'm Ahmed<br> -->
+               <h2> <span> المحامي المحلف أحمد الزيدي
+            </span></h2>
+            <p style="font-size:20px;"> يمكنك الآن حجز إستشارتك القانونية مع المحامي المحلف / أحمد الزيدي <br>
+<br>
+                الدفاع عن حقوق الناس
+               / حفظ المواد، والبنود، والقوانين
+                / عرض القضايا على القضاة والنيابة العامة 
+                <br> تقديم الحجج والأدلة والبراهين ومناقشتها
+                / تفسير وشرح القوانين، والقواعد، والتعليمات، والأحكام، والقرارات الصادرة
+                <br> تحضير وكتابة الوثائق والنصوص القانونية مثل العقود، والإبرامات القانونية، والوصايا، والدعاوى القضائية، وعقود التأمين
+                <br> تمثيل المتهمين في المحاكمات المدنية والجنائية وتقديم الأدلة في سبيل الدفاع عنهم وإثبات برائتهم
+                <br> تقديم النصائح، والإرشادات، والخدمات الاستشارية لموكّليهم
+                <br> تزويد الموكِّلين بأفضل السبل والطرق للتصرف بناءً على الحالات القانونية الفردية لكل فرد
+                <br> البحث في مُختلف القضايا القانونية
+                / الإشراف على المساعدين القانونيين
+                <br> السفر والتنقل من مكان إلى آخر لمقابلة الموكّلين
+                / حضور الجلسات في المحكمة
+                <br> فض النزاعات والخلافات / 
+                جمع البيانات والمعلومات وربطها ببعضها البعض
+                <br> الإلمام والمعرفة التامة بالقانون العام والقانون الخاص
+                <br> إعادة الحقوق المٌنتهكة إلى أصحابها
+                / التعامل مع القضايا المعقدة
+                / صنع القرارات واتخاذها
+                <br> القيام بالجهود المكثفة، والتحريات المتواصلة، والبحث، والاستشارات قبل تبرئة المتهم أمام القضاة في المحكمة
+                <br> شرح وتوضيح الحقوق المترتبة على المتهم
+                / حفظ وتوثيق المراجع القانونية
+                <br> تخمين وتقدير جميع الاحتمالات الوارد حدوثها فيما يخص القضية وإعلام الموكِّل بها
+                <br></ul>
+           <br> <br> <br>
+            </p>
+            <a href="https://wa.me/212671272638" class="main-btn">إستشر الان عبر واتساب!</a>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- <section class="cards" id="services">
+        <h2 class="title">Services</h2>
+        <div class="content">
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-edit"></i>
+                </div>
+                <div class="info">
+                    <h3>Script Writing</h3>
+                    <p>Research and write scripts for different topics that can then be directly recorded for video production</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-video"></i>
+                </div>
+                <div class="info">
+                    <h3>Video Production</h3>
+                    <p>Record and shoot videos taking into consideration proper settings, followed by production & post-production work</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-graduation-cap"></i>
+                </div>
+                <div class="info">
+                    <h3>Curriculum Development</h3>
+                    <p>Develop complete curriculums for various technical topics to produce digital transformative courses</p>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <!-- <section class="projects" id="projects">
+        <h2 class="title">Projects</h2>
+        <div class="content">
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="images/work1.jpg" />
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Script Writing & Video Production</p>
+                    <strong class="project-title">
+                        <span>Dev Books</span>
+                        <a href="#" class="more-details">More details</a>
+                    </strong>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="images/work2.jpg" />
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Script Writing & Video Production</p>
+                    <strong class="project-title">
+                        <span>Dev Books</span>
+                        <a href="#" class="more-details">More details</a>
+                    </strong>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="images/work3.jpg" />
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Script Writing & Video Production</p>
+                    <strong class="project-title">
+                        <span>Dev Books</span>
+                        <a href="#" class="more-details">More details</a>
+                    </strong>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="images/work4.jpg" />
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Script Writing & Video Production</p>
+                    <strong class="project-title">
+                        <span>Dev Books</span>
+                        <a href="#" class="more-details">More details</a>
+                    </strong>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="images/work5.jpg" />
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Script Writing & Video Production</p>
+                    <strong class="project-title">
+                        <span>Dev Books</span>
+                        <a href="#" class="more-details">More details</a>
+                    </strong>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="images/work6.jpg" />
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Script Writing & Video Production</p>
+                    <strong class="project-title">
+                        <span>Internet</span>
+                        <a href="#" class="more-details">More details</a>
+                    </strong>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <section class="cards contact" id="contact">
+        <h2 class="title">تواصل معنا</h2>
+        <div class="content">
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-phone"></i>
+                </div>
+                <div dir="rtl" class="info">
+                    <h3>Phone</h3>
+                    <p>38 26 27 71 6 212+</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <div class="info">
+                    <h3>Email</h3>
+                    <p>Ahmedzaidi@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- <footer class="footer">
+        <p class="footer-title">Copyrights @ <span>Islam Mahfouz</span></p>
+        <div class="social-icons">
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+    </footer> -->
+
+</body>
+</html>
